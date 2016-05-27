c640a57618e0dd9f6385efd57502957803ee7566
Jean-Pierre Gassin <jeanpierre.gassin@xcommedia.com.au>
2016-05-27 11:10:08 +1000

    add HelloWorld class and including PHPUnit and adding HelloWorldTest

---
+++ src/HelloWorld.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Jean_PierreGassin;
+
+/**
+ * A class to say hello
+ */
+class HelloWorld
+{
+	/** @var string $name Who to say hello to */
+	protected $name;
+
+	/**
+	 * Create a new hello world class
+	 *
+	 * @param string $name Who to say hello to, defaults to 'world'
+	 */
+	public function __construct($name = 'world')
+	{
+		$this->setName($name);
+	}
+
+	/**
+	 * Get name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Set name
+	 *
+	 * @param string $name Set name of who we are saying hello to
+	 * @return void
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
+
+	/**
+	 * Say Hello to name
+	 *
+	 * @return void
+	 */
+	public function say()
+	{
+		print "Hello $name!";
+	}
+}
