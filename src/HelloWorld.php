5d06bae3290eeefc29b6b7642109f5255c5ca9fe
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-09-15 22:22:05 +1000

    Update indentation to PSR standards

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -3,51 +3,51 @@
 namespace Jean_PierreGassin\HelloWorld;
 
 /**
- * A class to say hello
- */
+* A class to say hello
+*/
 class HelloWorld
 {
-	/** @var string $name Who to say hello to */
-	protected $name;
+    /** @var string $name Who to say hello to */
+    protected $name;
 
-	/**
-	 * Create a new hello world class
-	 *
-	 * @param string $name Who to say hello to, defaults to 'world'
-	 */
-	public function __construct($name = 'world')
-	{
-		$this->setName($name);
-	}
+    /**
+    * Create a new hello world class
+    *
+    * @param string $name Who to say hello to, defaults to 'world'
+    */
+    public function __construct($name = 'world')
+    {
+        $this->setName($name);
+    }
 
-	/**
-	 * Get name
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return $this->name;
-	}
+    /**
+    * Get name
+    *
+    * @return string
+    */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * Set name
-	 *
-	 * @param string $name Set name of who we are saying hello to
-	 * @return void
-	 */
-	public function setName($name)
-	{
-		$this->name = $name;
-	}
+    /**
+    * Set name
+    *
+    * @param string $name Set name of who we are saying hello to
+    * @return void
+    */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 
-	/**
-	 * Say Hello to name
-	 *
-	 * @return void
-	 */
-	public function say()
-	{
-		echo "Hello $this->name!";
-	}
+    /**
+    * Say Hello to name
+    *
+    * @return void
+    */
+    public function say()
+    {
+        echo "Hello $this->name!";
+    }
 }
--- tests/unit/HelloWorldTest.php
+++ tests/unit/HelloWorldTest.php
@@ -4,34 +4,34 @@ use Jean_PierreGassin\HelloWorld\HelloWorld;
 
 class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
-	public function testSayPrintsHelloWorld()
-	{
-		$this->expectOutputString('Hello world!');
-		(new HelloWorld())->say();
-	}
+    public function testSayPrintsHelloWorld()
+    {
+        $this->expectOutputString('Hello world!');
+        (new HelloWorld())->say();
+    }
 
-	public function testConstructorSetsName()
-	{
-		$name = 'name';
-		$helloWorld = new HelloWorld($name);
+    public function testConstructorSetsName()
+    {
+        $name = 'name';
+        $helloWorld = new HelloWorld($name);
 
-		$this->assertAttributeEquals($name, 'name', $helloWorld);
-	}
+        $this->assertAttributeEquals($name, 'name', $helloWorld);
+    }
 
-	public function testSetNameSetsName()
-	{
-		$name = 'name';
-		$helloWorld = new HelloWorld();
+    public function testSetNameSetsName()
+    {
+        $name = 'name';
+        $helloWorld = new HelloWorld();
 
-		$helloWorld->setName($name);
-		$this->assertAttributeEquals($name, 'name', $helloWorld);
-	}
+        $helloWorld->setName($name);
+        $this->assertAttributeEquals($name, 'name', $helloWorld);
+    }
 
-	public function testGetNameGetsName()
-	{
-		$name = 'world';
-		$helloWorld = new HelloWorld($name);
+    public function testGetNameGetsName()
+    {
+        $name = 'world';
+        $helloWorld = new HelloWorld($name);
 
-		$this->assertEquals($name, $helloWorld->getName());
-	}
+        $this->assertEquals($name, $helloWorld->getName());
+    }
 }
